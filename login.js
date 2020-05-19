32fb4c458bdffab35bb2b0c3b6bac3304c83336d
unknown <rush.nick@gmail.com>
2020-05-19 15:54:56 -0300

    image

---
+++ login.js
@@ -0,0 +1,75 @@
+$("#signup").click(function() {
+  $("#first").fadeOut("fast", function() {
+    $("#second").fadeIn("fast");
+  });
+});
+  
+$("#signin").click(function() {
+  $("#second").fadeOut("fast", function() {
+    $("#first").fadeIn("fast");
+  });  
+});
+  
+    
+           $(function() {
+             $("form[name='login']").validate({
+               rules: {
+                 
+                 email: {
+                   required: true,
+                   email: true
+                 },
+                 password: {
+                   required: true,
+                   
+                 }
+               },
+                messages: {
+                 email: "Por favor, digite seu e-mail",
+                
+                 password: {
+                   required: "Por favor, digite sua senha",
+                  
+                 }
+                 
+               },
+               submitHandler: function(form) {
+                 form.submit();
+               }
+             });
+           });
+           
+  
+  
+  $(function() {
+    
+    $("form[name='registration']").validate({
+      rules: {
+        firstname: "required",
+        lastname: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 6
+        }
+      },
+      
+      messages: {
+        firstname: "Por favor, digite seu nome",
+        lastname: "Por favor, digite seu sobrenome",
+        password: {
+          required: "Por favor, digite uma senha",
+          minlength: "Sua senha deve ter ao menos 6 digitos"
+        },
+        email: "Por favor, digite um e-mail válido"
+      },
+    
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+  
