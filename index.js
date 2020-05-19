7139e196a72acb757da351d2db8f572db85ba77f
unknown <rush.nick@gmail.com>
2020-05-19 10:27:41 -0300

    first commit with something inside

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import express from 'express';
-
-const app = express();
-
+const express = require('express')
+const app = express()
+ 
 app.listen(3000, () =>
-    console.log('Listening on port 3000 !'),
-    
+  console.log('Example app listening on port 3000!'),
 );
-
