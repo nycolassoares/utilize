f3f0603b4f99c59058b2d8e013db96ba14b912da
unknown <rush.nick@gmail.com>
2020-05-27 21:20:23 -0300

    login validation

--- login.js
+++ login.js
@@ -1,74 +1,8 @@
-$("#signup").click(function () {
-  $("#first").fadeOut("fast", function () {
-    $("#second").fadeIn("fast");
-  });
-});
+const express = require("express");
+const bodyParser = require("body-parser"); // inicializar app express
+const app = express();
 
-$("#signin").click(function () {
-  $("#second").fadeOut("fast", function () {
-    $("#first").fadeIn("fast");
-  });
-});
-
-
-$(function () {
-  $("form[name='login']").validate({
-    rules: {
-
-      email: {
-        required: true,
-        email: true
-      },
-      password: {
-        required: true,
-
-      }
-    },
-    messages: {
-      email: "Por favor, digite seu e-mail",
-
-      password: {
-        required: "Por favor, digite sua senha",
-
-      }
-
-    },
-    submitHandler: function (form) {
-      form.submit();
-    }
-  });
-});
-
-
-
-$(function () {
-
-  $("form[name='registration']").validate({
-    rules: {
-      firstname: "required",
-      lastname: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      password: {
-        required: true,
-        minlength: 6
-      }
-    },
-
-    messages: {
-      firstname: "Por favor, digite seu nome",
-      lastname: "Por favor, digite seu sobrenome",
-      password: {
-        required: "Por favor, digite uma senha",
-        minlength: "Sua senha deve ter ao menos 6 digitos"
-      },
-      email: "Por favor, digite um e-mail válido"
-    },
-
-    submitHandler: function (form) {
-      form.submit();
-    }
-  });
+let port = 8000;
+app.listen(port, () => {
+  console.log("Server listening on port" + port);
 });
